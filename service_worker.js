45a145ccdcf0a757eeddeed9ebd08173641ad7e2
m0x0m-m0x0m <xxphylaxx@gmail.com>
2025-10-18 11:21:18 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,32 +1,47 @@
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');  // サービスワーカーがインストール中であることを確認
+  console.log('Service Worker installing...');
   event.waitUntil(
     caches.open('my-cache').then((cache) => {
-      console.log('Caching resources...');  // リソースがキャッシュに追加されていることを確認
+      console.log('Caching resources...');
       return cache.addAll([
-        './',
-        '/index.html',
+        '/.',  // パスを絶対にする
+        '/style.css',
         '/button.png',
         '/hazure.png',
         '/atari.mp4',
-      ]);
+        '/manifest.json'  // マニフェストもキャッシュに追加
+      ]).catch((error) => {
+        console.error('キャッシュの追加中にエラーが発生しました:', error);
+      });
     })
   );
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated.');  // サービスワーカーがアクティベートされた時
+  console.log('Service Worker activated.');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'my-cache') {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Fetching: ', event.request.url);  // どのリクエストがキャッシュされているか、またはネットワークから取得されているか
+  console.log('Fetching: ', event.request.url);
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
-        console.log('Returning from cache: ', event.request.url);  // キャッシュからデータを返す
+        console.log('Returning from cache: ', event.request.url);
         return response;
       }
-      console.log('Fetching from network: ', event.request.url);  // ネットワークからデータを取得
+      console.log('Fetching from network: ', event.request.url);
       return fetch(event.request);
     })
   );
