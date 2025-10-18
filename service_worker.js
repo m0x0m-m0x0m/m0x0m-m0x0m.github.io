7bb9019baa7a49f73b10f15c31a3ba81a9e503f9
m0x0m-m0x0m <xxphylaxx@gmail.com>
2025-10-18 11:14:07 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,29 +1,34 @@
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
+  console.log('Service Worker installing...');  // サービスワーカーがインストール中であることを確認
   event.waitUntil(
     caches.open('my-cache').then((cache) => {
+      console.log('Caching resources...');  // リソースがキャッシュに追加されていることを確認
       return cache.addAll([
-        '/',
-        '/index.html',  // トップページをキャッシュに追加
+        '/index.html',
         '/images/icon-192x192.jpg',
         '/images/icon-512x512.jpg',
         '/button.png',
         '/hazure.png',
         '/atari.mp4',
-        // 他のファイルもここに追加
       ]);
     })
   );
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated.');
+  console.log('Service Worker activated.');  // サービスワーカーがアクティベートされた時
 });
 
 self.addEventListener('fetch', (event) => {
+  console.log('Fetching: ', event.request.url);  // どのリクエストがキャッシュされているか、またはネットワークから取得されているか
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        console.log('Returning from cache: ', event.request.url);  // キャッシュからデータを返す
+        return response;
+      }
+      console.log('Fetching from network: ', event.request.url);  // ネットワークからデータを取得
+      return fetch(event.request);
     })
   );
 });
